7c6a2e25c5125289bc3e6bb16034be6560be7d26
Ashish Chourasiya <ashishchourasiya2828@gmai.com>
2025-05-30 01:23:59 +0530

    Completed

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,11 +1,44 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import Button from "./Button";
 
-const ImageClipBox = ({ src, clipClass }) => (
-  <div className={clipClass}>
+
+
+export const ImageClipBox = ({ src, clipClass = "" }) => {
+  const [transformStyle, setTransformStyle] = useState("");
+  const itemRef = useRef(null);
+
+  const handleMouseMove = (event) => {
+    if (!itemRef.current) return;
+
+    const { left, top, width, height } =
+      itemRef.current.getBoundingClientRect();
+
+    const relativeX = (event.clientX - left) / width;
+    const relativeY = (event.clientY - top) / height;
+
+    const tiltX = (relativeY - 0.5) * 20;
+    const tiltY = (relativeX - 0.5) * -20;
+
+    const newTransform = `perspective(700px) rotateX(${tiltX}deg) rotateY(${tiltY}deg) scale3d(.98, .98, .98)`;
+    setTransformStyle(newTransform);
+  };
+
+  const handleMouseLeave = () => {
+    setTransformStyle("");
+  };
+
+  return (
+    <div
+      ref={itemRef}
+      className={clipClass}
+      onMouseMove={handleMouseMove}
+      onMouseLeave={handleMouseLeave}
+      style={{ transform: transformStyle }}
+    >
     <img src={src} alt="" />
   </div>
-);
+  );
+};
 
 const Contact = () => {
   return (
